0650652d0e903613b269097034a768eba8e93eab
HTaamas <96864185+HTaamas@users.noreply.github.com>
2024-03-19 22:55:28 +0100

    Innitial commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'neutral-700': '#2E2E2E',
+			},
+		},
 	},
 	plugins: [],
 }
